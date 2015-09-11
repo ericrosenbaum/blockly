1f38be7b78ad6e5e0f27a564711826d400831057
Eric Rosenbaum <eric.rosenbaum@gmail.com>
2015-09-11 00:34:01 -0400

    pre-scheduled notes with highlighting, added demos

--- blocks/synth.js
+++ blocks/synth.js
@@ -3,6 +3,11 @@ Blockly.Blocks['synth_play_note'] = {
     this.appendValueInput("pitch")
         .setCheck("Number")
         .appendField("play note");
+    this.appendValueInput("duration")
+        .setCheck("Number")
+        .appendField("for");
+    this.appendDummyInput()
+        .appendField("beats");
     this.setInputsInline(true);
     this.setPreviousStatement(true);
     this.setNextStatement(true);
@@ -11,3 +16,26 @@ Blockly.Blocks['synth_play_note'] = {
     this.setHelpUrl('');
   }
 };
+
+Blockly.Blocks['synth_voice'] = {
+  init: function() {
+    this.appendStatementInput("VOICE")
+        .setCheck(null)
+        .appendField("start voice");
+    this.setColour(230);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+/*
+Blockly.Blocks['synth_every_bar'] = {
+  init: function() {
+    this.appendStatementInput("on every bar")
+        .setCheck(null)
+        .appendField(new Blockly.FieldDropdown([["every bar", "ONEBAR"], ["every 2 bars", "TWOBARS"], ["every 4 bars", "FOURBARS"]]), "INTERVAL");
+    this.setTooltip('');
+    this.setHelpUrl('');
+  }
+};
+*/
--- generators/javascript/synth.js
+++ generators/javascript/synth.js
@@ -1,9 +1,30 @@
 Blockly.JavaScript['synth_play_note'] = function(block) {
 
 	var value_pitch = Blockly.JavaScript.valueToCode(block, 'pitch', Blockly.JavaScript.ORDER_ATOMIC);
-	
-	code = 'playNote(' + value_pitch + ');\n';
+	var value_duration = Blockly.JavaScript.valueToCode(block, 'duration', Blockly.JavaScript.ORDER_ATOMIC);
+
+	value_pitch = value_pitch ? value_pitch : 'null';
+	value_duration = value_duration ? value_duration : 'null';
+
+	code = 'playNote(' + value_pitch + ', ' + value_duration + ', ' + block.id + ');\n';
 
 	return code;
 }
 
+Blockly.JavaScript['synth_voice'] = function(block) {
+  var statements_voice = Blockly.JavaScript.statementToCode(block, 'VOICE');
+  var code = 'setTime(0);\n';
+  code += statements_voice;
+  return code;
+};
+
+/*
+Blockly.JavaScript['synth_every_bar'] = function(block) {
+  var dropdown_interval = block.getFieldValue('INTERVAL');
+  var statements_on_every_bar = Blockly.JavaScript.statementToCode(block, 'on every bar');
+  var code = '{\n';
+  code += statements_on_every_bar;
+  code += '}\n';
+  return code;
+};
+*/
